ccd623ef9af73b82ee4533e22ed618fea6c848f2
SpooTo <milonskola@gmail.com>
2016-05-18 00:18:40 +0200

    first
    mvc - time and date

---
+++ vs_tutorialspoint_mvc/vs_tutorialspoint_mvc/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace vs_tutorialspoint_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            int hour = DateTime.Now.Hour;
+
+            if (hour < 12)
+            {
+                ViewBag.Greeting = "Good Morning. The time is : " + DateTime.Now.ToShortTimeString();
+            }
+            else
+            {
+                ViewBag.Greeting = "Good Afternoon. The time is : " + DateTime.Now.ToShortTimeString();
+            }
+
+            ViewBag.Date = "And the date is : " + DateTime.Now.ToShortDateString();
+
+            return View();
+        }
+    }
+}
---
+++ vs_tutorialspoint_mvc/vs_tutorialspoint_mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="vs_tutorialspoint_mvc.MvcApplication" Language="C#" %>
